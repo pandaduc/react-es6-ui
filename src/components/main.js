38975042c3242777d291fd71e04348fb769d4932
Duc Nguyen <duc.nguyen@elanmediapartners.com>
2016-04-07 12:36:15 +1000

    remove --hot from npm start

--- src/components/main.js
+++ src/components/main.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class Main extends Component {
     render() {
         return (
-            <h1>Hello World</h1>
+            <h1>Hello World!</h1>
         )
     }
 }
